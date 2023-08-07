231cc22cfa1f2a5c23c0733172b9795b6dddc9b8
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-08-07 16:33:13 +0530

    login and signup page creted

--- src/app/Modules/User/user.module.ts
+++ src/app/Modules/User/user.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common'
 import { UserRoutingModule } from './user-routing.module'
 import { UserManagerComponent } from './user-manager.component'
 import { NavbarComponent } from './Pages/Layout/navbar/navbar.component';
-import { PageManagerComponent } from './Pages/LandingPage/page-manager/page-manager.component'
+import { PageManagerComponent } from './Pages/LandingPage/page-manager/page-manager.component';
+import { LoginComponent } from './Pages/Auth/login/login.component';
+import { RegisterComponent } from './Pages/Auth/register/register.component'
 
 @NgModule({
-  declarations: [UserManagerComponent, NavbarComponent, PageManagerComponent],
+  declarations: [UserManagerComponent, NavbarComponent, PageManagerComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule, UserRoutingModule]
 })
 export class UserModule {}
