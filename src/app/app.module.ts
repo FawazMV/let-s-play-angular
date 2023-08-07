0963dd01932766f708df88d56ceadcd72519374b
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-08-07 16:08:48 +0530

    ngrx installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { UserModule } from './Modules/User/user.module'
+import { UserModule } from './Modules/User/user.module';
+import { StoreModule } from '@ngrx/store'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent]
 })
